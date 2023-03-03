7e87e5a3ac630434d85f84a4023c646d5d859a16
RashidaAvshar <rashidaavshar@gmail.com>
2023-03-03 14:53:45 +0400

    finish

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -14,8 +14,6 @@ const Register = () => {
     username: "",
     password: "",
     id: Date.now()
-// bir dene onu bura elave eledin? he, amma gorunur, o pis haldi nece ki? gorunmemelidi ki? eslinde yox, bu sadece unikallig ucun lazimdi eslinde
-// basa dusmedim nei deirsen nie gorunmemelidi/? id bu formada
   })
 
   const onHandleChange = (e) =>{
@@ -28,7 +26,12 @@ const Register = () => {
         swal("Good job!", "You clicked the button!", "success");
       }
     })
-    console.log("nhkj", userData)
+    {
+      document.querySelector("#name").value="";
+      document.querySelector("#surname").value="";
+      document.querySelector("#username").value="";
+      document.querySelector("#password").value="";
+    }
   }
   
   const {name, surname, username, password} = userData
@@ -37,15 +40,16 @@ const Register = () => {
         <div className="register-box">
             <div className="register">
               <h1>REGISTER</h1>
-            <TextField id="standard-basic" label="Name" variant="standard" name="name" onChange={onHandleChange} />
-            <TextField id="standard-basic" label="Surname" variant="standard"name="surname" onChange={onHandleChange} />
-            <TextField id="standard-basic" label="Username" variant="standard" name="username" onChange={onHandleChange}/>
-            <TextField id="standard-basic" label="Password" variant="standard" name="password" type="password" onChange={onHandleChange}/>
-            <Button variant="contained" color="success" onClick={onHandleClick} disabled={name==""|| surname=="" || username=="" || password.length <= 8 ? true: false} >register</Button>
+            <TextField id="name" label="Name" variant="standard" name="name" onChange={onHandleChange} />
+            <TextField id="surname" label="Surname" variant="standard"name="surname" onChange={onHandleChange} />
+            <TextField id="username" label="Username" variant="standard" name="username" onChange={onHandleChange}/>
+            <TextField id="password" label="Password" variant="standard" name="password" type="password" onChange={onHandleChange}/>
+            <Button variant="contained" color="success" onClick={onHandleClick} 
+            disabled={name==""|| surname=="" || username=="" || password.length <= 8 ? true: false} >register</Button>
             </div>
-        </div>
-      
+        </div>      
     </div>
+
   )
 }
 
